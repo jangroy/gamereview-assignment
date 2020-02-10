9d382d0cae31d711ae0cf0f75d5ab08ead7de488
Roy Jang <jangroy@hotmail.com>
2020-02-09 21:51:06 -0800

    update app.listen message and add polyfill to app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
+import '@babel/polyfill';
 import 'dotenv/config';
-
 const app = require('./server/server');
 const PORT = process.env.PORT || 3001;
 
-// helps to not open port when testing
+// while testing, app.listen() within the test suite
 if (process.env.NODE_ENV !== 'test') {
   app.listen(PORT);
-  console.log(`Server is live at localhost:${PORT}`);
+  console.log(`Server is live at Port:${PORT}`);
 }
 
 module.exports = app;
