cc6a25d54c50a693371608694d15673599c0770a
Roy Jang <jangroy@hotmail.com>
2020-02-14 22:57:30 -0800

    add frontend with create-react-app

---
+++ client/src/Client.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { EuiPanel } from '@elastic/eui';
+
+const Client = () => <EuiPanel>React App!</EuiPanel>;
+
+export default Client;
---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Client from './Client';
+import '@elastic/eui/dist/eui_theme_light.css';
+import './index.css';
+
+ReactDOM.render(<Client />, document.getElementById('root'));
